8e30cf04a1cdde472c07f0bb1bede4fd324e8b0d
philipmir <philipmir@hotmail.com>
2023-05-01 15:57:49 +0200

    first commit

---
+++ SwiftUI-Coredata-intro/ShoppingListView.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingListView.swift
+//  SwiftUI-Coredata-intro
+//
+//  Created by Philip Miranda on 2023-05-01.
+//
+
+import SwiftUI
+
+struct ShoppingListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShoppingListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoppingListView()
+    }
+}
